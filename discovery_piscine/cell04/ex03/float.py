9a95ad1be1e148964c1d603b8cbff1b348c2d576
KanpitchaKotchapoom <141375849+KanpitchaKotchapoom@users.noreply.github.com>
2026-09-24 20:11:17 +0700

    Update float.py

--- discovery_piscine/cell04/ex03/float.py
+++ discovery_piscine/cell04/ex03/float.py
@@ -1,2 +1,2 @@
 text = input("Give me a number: ")
-print("This number is an integer." if text.isdecimal() else "This number is a decimal.")
+print("This number is an integer." if text.isdecimal() or float(text).is_integer() else "This number is a decimal.")
